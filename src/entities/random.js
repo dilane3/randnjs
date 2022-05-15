5f1372ddcbac699723129f6a10066311fb50d712
dilane3 <komboudilane125@gmail.com>
2022-05-16 00:04:03 +0100

    ADD: Add method tha generate a list of a number of integers included between min and max values

--- src/entities/random.js
+++ src/entities/random.js
@@ -33,7 +33,59 @@ class Random {
    * @returns number
    */
   static generate (min = this.min, max = this.max) {
-    return Math.floor(this.initialValueGenerated() * max + 1) + min
+    if (typeof min === "number" && typeof max === "number") {
+      return Math.floor(this.initialValueGenerated() * max + 1) + min
+    }
+
+    throw new Error("min and max parameters have to be numbers")
+  }
+
+  /**
+   * Generate a list of numbers
+   * @param {number} length 
+   * @param {number} min 
+   * @param {number} max 
+   * @returns number
+   */
+  static samples (length = 1, min = this.min, max = this.max) {
+    if (typeof length === "number") {
+      // Initial list of numbers
+      const list = []
+
+      for (let i=0; i<length; i++) {
+        // Generate a number
+        const randomValue = this.generate(min, max)
+
+        list.push(randomValue)
+      }
+
+      return list
+    }
+
+    throw new Error("length parameter has to be number")
+  }
+
+  /**
+   * Choose a value from a list of values
+   * @param {Array} array 
+   * @returns number
+   */
+  static choice (array) {
+    if (array instanceof Array) {
+      if (array.length > 0) {
+        const minIndex = 0
+        const maxIndex = array.length - 1
+  
+        // Generate random index included between minIndex and maxIndex
+        const randomIndex = this.generate(minIndex, maxIndex)
+  
+        return array[randomIndex]
+      }
+
+      throw new Error("The argument of choice() method must not be to be an empty array")
+    } else {
+      throw new Error("The argument of choice() method has to be an array")
+    }
   }
 }
 
--- test/random.test.js
+++ test/random.test.js
@@ -18,19 +18,45 @@ describe("Random test", () => {
     return compare.lessThanOrEqual(value, max) && compare.greaterThanOrEqual(value, min)
   }
 
-  test("Simple generate method", () => {
+  test("Simple generation method with default min and max values", () => {
     expect(verification(Random.generate())).toBe(true)
     expect(typeof Random.generate() === 'number').toBe(true)
     expect(Random.generate()).toBeGreaterThan(Random.min)
     expect(Random.generate()).toBeLessThan(Random.max)
   })
 
-  test("Simple generate method with custom min and max value", () => {
+  test("Simple generation method with custom min and max values", () => {
     const min = 0
     const max = 10
+    const negativeMin = -100
 
+    expect(() => Random.generate("a", "b")).toThrow("min and max parameters have to be numbers")
     expect(verification(Random.generate(min, max), min, max)).toBe(true)
     expect(Random.generate(min, max)).toBeGreaterThan(min)
     expect(Random.generate(min, max)).toBeLessThan(max)
+    expect(Random.generate(negativeMin, max)).toBeGreaterThan(negativeMin)
+  })
+
+  test("Generation of a list of numbers", () => {
+    const min = 0
+    const max = 100
+    const length = 10
+
+    // Generate a list of numbers
+    const samples = Random.samples(length, min, max)
+
+    expect(samples.length).toEqual(length)
+    expect(samples.every(item => verification(item, min, max))).toBe(true)
+    expect(() => Random.samples("a")).toThrow("length parameter has to be number")
+  })
+
+  test("Choose a value randomly from a list of elements", () => {
+    const numbers = [1, 4, 6, 10, 12]
+    const fruits = ["apple", "banana", "orange", "pineapple", "watermelon"]
+
+    expect(numbers.includes(Random.choice(numbers))).toBe(true)
+    expect(fruits.includes(Random.choice(fruits))).toBe(true)
+    expect(() => Random.choice(1)).toThrow("The argument of choice() method has to be an array")
+    expect(() => Random.choice([])).toThrow("The argument of choice() method must not be to be an empty array")
   })
 })
